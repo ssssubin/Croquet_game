633f8c5b5f55ef6180999095470dcdf55e2a9849
ssssu <cdw3521@naver.com>
2024-04-10 17:50:05 +0900

    🐰main 구현 - 버튼 누르면 slide 넘기는 기능 구현 - tab 기능 구현

--- index.js
+++ index.js
@@ -6,3 +6,131 @@ for (let i = 0; i < headeraTag.length; i++) {
     window.scrollTo({ top: target.offsetTop, behavior: "smooth" });
   };
 }
+
+let slider = document.querySelector("#slider");
+let slides = slider.querySelector(".slides");
+let slide = slides.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+setInterval(function () {
+  let from = -(1024 * currentSlide);
+  let to = from - 1024;
+  slides.animate(
+    [
+      {
+        marginLeft: from + "px",
+      },
+      {
+        marginLeft: to + "px",
+      },
+    ],
+    {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both",
+    }
+  );
+  currentSlide++;
+  if (currentSlide === slide.length - 1) {
+    currentSlide++;
+  }
+}, 3000);
+
+let listaTag = document.querySelectorAll(".tabs-list li a");
+let listliTag = document.querySelectorAll(".tabs-list li");
+for (let i = 0; i < listaTag.length; i++) {
+  listaTag[i].onclick = function (e) {
+    e.preventDefault();
+  };
+}
+
+for (let i = 0; i < listliTag.length; i++) {
+  listliTag[i].onclick = function () {
+    let href = this.querySelector("a").getAttribute("href");
+    document.querySelectorAll(".tabs-list li").forEach((item) => {
+      item.classList.remove("active");
+    });
+    document.querySelectorAll(".tabs div.tab").forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    this.classList.add("active");
+    document.querySelector(href).classList.add("active");
+  };
+}
+
+let rightBtn = document.querySelector(".right-arrow");
+rightBtn.onclick = function () {
+  let currentImg = document.querySelector("#photo .slide.active");
+  let nextImg = currentImg.nextElementSibling;
+  if (nextImg === null) {
+    nextImg = currentImg.parentElement.firstElementChild;
+  }
+  currentImg.animate(
+    [
+      {
+        opacity: [1, 0],
+      },
+    ],
+    {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both",
+    }
+  );
+  currentImg.classList.remove("active");
+  nextImg.animate(
+    [
+      {
+        opacity: [0, 1],
+      },
+    ],
+    {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both",
+    }
+  );
+  nextImg.classList.add("active");
+};
+
+let leftBtn = document.querySelector(".left-arrow");
+leftBtn.onclick = function () {
+  let currentImg = document.querySelector("#photo .slide.active");
+  let previousImg = currentImg.previousElementSibling;
+  if (previousImg === null) {
+    previousImg = currentImg.parentElement.lastElementChild;
+  }
+  currentImg.animate(
+    [
+      {
+        opacity: [1, 0],
+      },
+    ],
+    {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both",
+    }
+  );
+  currentImg.classList.remove("active");
+  previousImg.animate(
+    [
+      {
+        opacity: [0, 1],
+      },
+    ],
+    {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both",
+    }
+  );
+  previousImg.classList.add("active");
+};
